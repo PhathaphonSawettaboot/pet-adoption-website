c68162b53538990427fe4031e76ce7e7aaab293e
Newlyn <phathaphon.s@gmail.com>
2024-10-17 18:54:48 +0800

    Almost finish the pets area

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const template = document.querySelector('#pet-card-template')
+const wrapper = document.createDocumentFragment()
+
 async function start() {
   const weatherPromise = await fetch('https://api.weather.gov/gridpoints/STO/29,164/forecast')
   const weatherData = await weatherPromise.json()
@@ -12,8 +15,24 @@ async function petsArea() {
   const petsAreaPromise = await fetch('https://learnwebcode.github.io/bootcamp-pet-data/pets.json')
   const PetsAreaData = await petsAreaPromise.json()
   PetsAreaData.forEach(pet => {
-    console.log(pet.species)
+    const clone = template.content.cloneNode(true)
+    clone.querySelector('h3').textContent = pet.name
+    clone.querySelector('.pet-description').textContent = pet.description
+    clone.querySelector('.pet-age').textContent = createAgeText(pet.birthYear)
+    clone.querySelector('.pet-card-photo img').src = pet.photo
+    clone.querySelector('.pet-card-photo img').alt = `A ${pet.species} named ${pet.name}`
+    wrapper.appendChild(clone)
   });
+  document.querySelector('.list-of-pets').appendChild(wrapper)
 }
 
 petsArea()
+
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear()
+  const age = currentYear - birthYear
+
+  if (age == 1) return '1 Year Old'
+  if (age == 0) return 'Less Than A Year Old'
+  if (age > 1) return `${age} Years Old`
+}
